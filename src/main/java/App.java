df7287a28bebfa9087c21b1611ff4f17ec0ca669
MoissBatt <valentine.bouche.stjo@gmail.com>
2022-12-03 00:14:07 +0100

    Prepare day 3

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,13 +1,14 @@
 import days.Day;
 import days.Day01;
 import days.Day02;
+import days.Day03;
 
 public class App {
 
     public static void main(String[] args) {
         System.out.println("\n>>>>>>>>>>>>>> ADVENT OF CODE <<<<<<<<<<<<<<\n");
 
-        Day day = new Day02();
+        Day day = new Day03();
         System.out.println(">>>>> ADVENT OF CODE -- DAY " + day.DATE + " -- START <<<<<\n");
         day.go();
         System.out.println("\n>>>>> ADVENT OF CODE --  DAY " + day.DATE + " --  END <<<<<");
---
+++ src/main/java/days/Day03.java
@@ -0,0 +1,12 @@
+package days;
+
+public class Day03 extends Day {
+
+    public Day03() {
+        super(3, "input_3.txt");
+    }
+
+    public void go() {
+
+    }
+}
