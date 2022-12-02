b7b24759afdb2ac62721206149b99a76ec2220fd
MoissBatt <valentine.bouche.stjo@gmail.com>
2022-12-02 09:42:17 +0100

    Day 2

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -9,15 +9,16 @@ import java.util.Optional;
 
 public class App {
 
-    public static void main(String[] args) throws IOException, URISyntaxException {
+    public static void main(String[] args) throws IOException {
         System.out.println("\n-------- ADVENT OF CODE --------\n");
-        dayOne();
+        FileReader fileReader = new FileReader();
+        dayOne(fileReader);
+        dayTwo(fileReader);
     }
 
-    public static void dayOne() throws IOException, URISyntaxException {
+    public static void dayOne(FileReader fileReader) throws IOException {
         System.out.println(">> ADVENT OF CODE -- DAY 1 -- START");
-        FileReader fileReader = new FileReader();
-        BufferedReader br = fileReader.fileReader();
+        BufferedReader br = fileReader.fileReader("input_1.txt");
 
         HashMap<Integer, Integer> elvesStocks = new HashMap<>();
         int key = 1;
@@ -65,7 +66,46 @@ public class App {
         System.out.println(">> ADVENT OF CODE -- DAY 1 -- END\n");
     }
 
-    public static void dayTemplate() {
+    public static void dayTwo(FileReader fileReader) throws IOException {
+        System.out.println(">> ADVENT OF CODE -- DAY 2 -- START");
+        BufferedReader br = fileReader.fileReader("input_2.txt");
+
+        HashMap<String, Integer> gameModel = new HashMap<>();
+        gameModel.put("A X", 4);
+        gameModel.put("B X", 1);
+        gameModel.put("C X", 7);
+        gameModel.put("A Y", 8);
+        gameModel.put("B Y", 5);
+        gameModel.put("C Y", 2);
+        gameModel.put("A Z", 3);
+        gameModel.put("B Z", 9);
+        gameModel.put("C Z", 6);
+
+        HashMap<String, Integer> gameModel2 = new HashMap<>();
+        gameModel2.put("A X", 3);
+        gameModel2.put("B X", 1);
+        gameModel2.put("C X", 2);
+        gameModel2.put("A Y", 4);
+        gameModel2.put("B Y", 5);
+        gameModel2.put("C Y", 6);
+        gameModel2.put("A Z", 8);
+        gameModel2.put("B Z", 9);
+        gameModel2.put("C Z", 7);
+
+        int score = 0;
+        int score2 = 0;
+        String line;
+        while ((line = br.readLine())!= null){
+            score += gameModel.get(line);
+            score2 += gameModel2.get(line);
+        }
+        System.out.println("Score total : "+ score);
+        System.out.println("Score total 2 : "+ score2);
+
+        System.out.println(">> ADVENT OF CODE -- DAY 2 -- END\n");
+    }
+
+    public static void dayTemplate(FileReader fileReader) throws IOException  {
         System.out.println(">> ADVENT OF CODE -- DAY 1 -- START");
         System.out.println(">> ADVENT OF CODE -- DAY 1 -- END\n");
     }
--- src/main/java/utils/FileReader.java
+++ src/main/java/utils/FileReader.java
@@ -7,17 +7,22 @@ import java.net.URISyntaxException;
 import java.net.URL;
 
 public class FileReader {
-    public BufferedReader fileReader() throws IOException, URISyntaxException {
-        File file;
-        URL resource = getClass().getClassLoader().getResource("input_1.txt");
-        if (resource == null) {
-            throw new IllegalArgumentException("file not found!");
-        } else {
-            file = new File(resource.toURI());
+    public BufferedReader fileReader(String filename)  {
+        try {
+            File file;
+            URL resource = getClass().getClassLoader().getResource(filename);
+            if (resource == null) {
+                throw new IllegalArgumentException("file not found!");
+            } else {
+                file = new File(resource.toURI());
+            }
+            BufferedReader br = new BufferedReader(new java.io.FileReader(file));
+            return br;
+        }catch(IOException e) {
+            System.out.printf(e.getMessage());
+        } catch(URISyntaxException e){
+            System.out.printf(e.getMessage());
         }
-
-        BufferedReader br = new BufferedReader(new java.io.FileReader(file));
-
-        return br;
+        return null;
     }
 }
